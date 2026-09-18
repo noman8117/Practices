ac7325ad16f3a933f6c84a2bb95dd250bc43fbb0
Noman <noman@Nomans-MacBook-Air.local>
2026-09-18 23:44:33 +0600

    Add new Java practice

---
+++ BEE_2748.java
@@ -0,0 +1,15 @@
+/**
+ * BEE_2748
+ */
+public class BEE_2748 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("|        Roberto                      |");
+        System.out.println("|                                     |");
+        System.out.println("|        5786                         |");
+        System.out.println("|                                     |");
+        System.out.println("|        UNIFEI                       |");
+        System.out.println("---------------------------------------");
+    }
+}
